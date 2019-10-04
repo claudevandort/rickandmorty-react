6775b0375c3b8d706b5724b9357b5b1dcb512be6
Claudio Guerra <claudevandort@gmail.com>
2019-10-04 13:20:37 -0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Character from './components/Character'
 
-ReactDOM.render(<Character name="Rick Sanchez" />, document.getElementById(app))
+ReactDOM.render(<Character name="Rick Sanchez" />, document.getElementById('app'))
