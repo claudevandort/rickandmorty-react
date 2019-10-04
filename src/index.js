dd70937a99e3c8e8aaf83aef33b429b30f42fed0
Claudio Guerra <claudevandort@gmail.com>
2019-10-04 13:03:05 -0300

    render component

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Character from './components/Character'
+
+ReactDOM.render(<Character name="Rick Sanchez" />, document.getElementById(app))
